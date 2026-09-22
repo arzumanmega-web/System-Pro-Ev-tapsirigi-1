263a78d105570fb7cef439c6d561e0837c2e6a63
arzum <arzum@DESKTOP-V39TLP5>
2026-09-22 23:56:41 +0400

    Add project files.

---
+++ System Pro Ev tapsirigi 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace System_Pro_Ev_tapsirigi_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
